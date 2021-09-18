260e340489654d1a9296150bc27cce7b6437beb4
Sota-k <violetdev9966@gmail.com>
2021-09-18 22:04:39 +0900

    CreateProject

---
+++ components/ToDoApp/Tasks.vue
@@ -0,0 +1,20 @@
+<template>
+    <v-card>
+    </v-card>
+</template>
+
+<script lang="ts">
+import {Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class Tasks extends Vue{
+
+}
+</script>
+
+
+
+
+<style>
+
+</style>
---
+++ pages/toDoApp.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <AddTaskDialog />
+        <v-container>
+        <v-card>
+            <v-card-title>
+                To-Do-App-Nuxt-Vuetify
+            </v-card-title>
+            <v-card-text>
+                <Tasks />
+            </v-card-text>
+        </v-card>
+        </v-container>
+    </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator'
+import tasks from '~/assets/json/tasks.json'
+import AddTaskDialog from '~/components/ToDoApp/AddTaskDialog.vue'
+import Tasks from'~/components/ToDoApp/Tasks.vue'
+
+@Component({
+    components: {
+        AddTaskDialog,
+        Tasks
+    }
+})
+
+export default class ToDoApp extends Vue{
+    header: object[] = tasks.task.headers
+    tasks: object[] = tasks.task.tasks;
+}
+</script>
+<style>
+
+</style>
